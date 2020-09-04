be90af0b02d87d2211f55e00be438a398e1cac80
wuyaheng <wuyaheng2016@gmail.com>
2020-09-04 13:28:22 -0400

    add nav and styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import SearchForm from './components/SearchForm/index'
 import ResultList from './components/ResultList/index'
 import axios from 'axios';
+import './App.css';
 
 
 class App extends Component {
